6a7979c1e6c3886c94eafe4c25c9b810212210d8
dremdeveloper <131899974+dremdeveloper@users.noreply.github.com>
2023-09-10 23:25:16 +0900

    Update 09.py

--- solution/09.py
+++ solution/09.py
@@ -1,10 +1,8 @@
 def solution(decimal):
-  stack = [ ]
-  while decimal > 0:
-    remainder = decimal % 2
-    stack.append(str(remainder))
-    decimal //= 2
-  binary = ""
-  while stack:
-    binary += stack.pop( ) 
-  return binary
+    stack = []
+    while decimal > 0:
+        remainder = decimal % 2
+        stack.append(str(remainder))
+        decimal //= 2
+    stack.reverse()
+    return ''.join(stack)
