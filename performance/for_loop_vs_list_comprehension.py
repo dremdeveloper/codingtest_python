2699574a34e6b438b2d8f5520af8480b6d27e512
dremdeveloper <131899974+dremdeveloper@users.noreply.github.com>
2023-09-13 00:48:42 +0900

    Update for_loop_vs_list_comprehension.py

--- performance/for_loop_vs_list_comprehension.py
+++ performance/for_loop_vs_list_comprehension.py
@@ -14,5 +14,5 @@ start_time = time.time()
 squared_numbers_comprehension = [i * i for i in range(num_elements)]
 list_comprehension_time = time.time() - start_time
 
-print(f"Using for loop took: {for_loop_time:.6f} seconds") # 1.347505(환경마다 다를 수 있음)
+print(f"Using for loop took: {for_loop_time:.6f} seconds") # 1.347505초(환경마다 다를 수 있음)
 print(f"Using list comprehension took: {list_comprehension_time:.6f} seconds") # 0.536171초(환경마다 다를 수 있음)
