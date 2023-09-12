8e16f564688c995620ced41b2d316c6626aa768f
dremdeveloper <131899974+dremdeveloper@users.noreply.github.com>
2023-09-13 00:55:33 +0900

    Update string_concatenation_performance.py

--- performance/string_concatenation_performance.py
+++ performance/string_concatenation_performance.py
@@ -1,7 +1,7 @@
 import time
 
 num_elements = 1000000
-strings = ["a"] * num_elements
+strings = ["abcd"] * num_elements
 
 # += 연산자 사용
 start_time = time.time()
@@ -15,5 +15,5 @@ start_time = time.time()
 result_str = "".join(strings)
 join_time = time.time() - start_time
 
-print(f"Using += operator took: {plus_equals_time:.6f} seconds")
-print(f"Using join() method took: {join_time:.6f} seconds")
+print(f"Using += operator took: {plus_equals_time:.6f} seconds")# 0.514103초(환경 마다 다를 수 있음)
+print(f"Using join() method took: {join_time:.6f} seconds")# 0.020838초(환경 마다 다를 수 있음)
