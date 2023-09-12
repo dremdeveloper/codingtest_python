b4b642b7fbf421a2f3201ad3b3607ee8dc5e4791
dremdeveloper <131899974+dremdeveloper@users.noreply.github.com>
2023-09-13 01:51:54 +0900

    Update 47.py

--- solution/47.py
+++ solution/47.py
@@ -8,12 +8,9 @@ def solution(N):
 
     for i in range(start, N + 1):  # ❸ 다음에 선택할 수 있는 숫자들을 하나씩 선택하면서
       if sum + i <= 10:  # ❹ 선택한 숫자의 합이 10보다 작거나 같으면
-        if (
-          i not in selected_nums
-        ):  # ❺ 같은 숫자는 한 번만 선택할 수 있으므로, 이미 선택한 숫자는 선택하지 않습니다.
           backtrack(
             sum + i, selected_nums + [i], i + 1
-          )  # ❻ 백트래킹 함수를 재귀적으로 호출합니다.
+          )  # ❺ 백트래킹 함수를 재귀적으로 호출합니다.
 
-  backtrack(0, [], 1)  # ❼ 백트래킹 함수 호출
-  return results  # ❽ 조합 결과 반환
+  backtrack(0, [], 1)  # ❻ 백트래킹 함수 호출
+  return results  # ❼ 조합 결과 반환
