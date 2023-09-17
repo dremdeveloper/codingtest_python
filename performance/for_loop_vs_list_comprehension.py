bbb53e125b0ae5a4de083eef32383fa4a3e7c614
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-09-17 23:15:16 +0900

    Update for_loop_vs_list_comprehension.py

--- performance/for_loop_vs_list_comprehension.py
+++ performance/for_loop_vs_list_comprehension.py
@@ -1,3 +1,14 @@
+#############################################################
+# | cafe       | http://cafe.naver.com/dremdelover          |
+# | Q&A        | https://open.kakao.com/o/gX0WnTCf          |
+# | business   | ultrasuperrok@gmail.com                    |
+#############################################################
+# 1. for 루프 사용: 이 부분에서는 for 루프를 사용하여 각 숫자의 제곱을 계산하고, 이를 squared_numbers 리스트에 하나씩 추가합니다. 
+#    이 과정에서 반복문을 통해 반복적인 메서드 호출이 발생하며, 일반적으로 시간 복잡도는 O(n)입니다.
+#
+# 2. list comprehension 사용: list comprehension은 for 루프를 사용하는 것보다 더 빠른 방법으로 리스트를 생성할 수 있습니다. 
+#    이는 내부적으로 최적화가 더 잘 되어 있어 연산이 더 빠르게 진행되기 때문입니다. 
+#    이 방법 역시 시간 복잡도는 O(n)이지만, 상수 시간 요소가 for 루프보다 작아 성능이 더 좋습니다.
 import time
 
 num_elements = 10000000
