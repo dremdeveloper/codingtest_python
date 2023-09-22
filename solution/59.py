cfb23d82b1ee7d487ebe644b6dde71ae4157ec27
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-09-22 18:01:26 +0900

    Update 59.py

--- solution/59.py
+++ solution/59.py
@@ -5,12 +5,13 @@ def compare(a, b):
   # 예. a=3, b=30 -> t1='330', t2='303' -> int(t1) > int(t2) -> 1 반환
   t1 = str(a) + str(b)
   t2 = str(b) + str(a)
+  # t1이 크다면 1, t2가 크다면 -1, 같으면 0
   return (int(t1) > int(t2)) - (int(t1) < int(t2))
 
 def solution(numbers):
   # ➋ reverse = True를 이용해 내림차순으로 정렬합니다.
   sorted_nums = sorted(
-    numbers, key=functools.cmp_to_key(lambda a, b: compare(a, b)), reverse=True
+    numbers, key=functools.cmp_to_key(compare), reverse=True
   )
   # ➌ 각 정렬된 수를 문자열로 이어붙인 뒤, int로 변환한 값을 문자열로 다시 변환하여 반환합니다.
   answer = "".join(str(x) for x in sorted_nums)
