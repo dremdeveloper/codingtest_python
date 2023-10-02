4a164412a34b2a8c2486d6ad7790e7dedab6630d
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-10-02 16:23:47 +0900

    Update dfs_stack.py

--- Algorithm_with_DataStructure/dfs_stack.py
+++ Algorithm_with_DataStructure/dfs_stack.py
@@ -33,12 +33,14 @@ def dfs(graph, start_node):
     
     while stack:
         node = stack.pop()
-        if not visited[node] :
-            visited[node] = True
-            print(node)
-            stack.extend(reversed(graph[node]))  # reversed를 사용하여 깊이 우선 탐색을 유지
-            
-            
+        visited[node] = True;
+        print(node)
+        
+        for adj_node in graph[node]: # 인접한 노드 방문
+           if not visited[adj_node]:
+              stack.append(adj_node)
+              
+
 # 그래프를 인접 리스트로 표현
 graph = {
     1: [2, 3],
@@ -49,4 +51,4 @@ graph = {
 }
 
 # DFS 알고리즘 실행
-dfs(graph, 1) # 1 2 4 5 3
+dfs(graph, 1) # 1 3 2 5 4
