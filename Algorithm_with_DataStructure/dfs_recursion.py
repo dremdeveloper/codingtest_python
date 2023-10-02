d87e1a4c6b040ee2d09dd401a9260811b328ce91
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-10-02 16:28:47 +0900

    Update dfs_recursion.py

--- Algorithm_with_DataStructure/dfs_recursion.py
+++ Algorithm_with_DataStructure/dfs_recursion.py
@@ -21,14 +21,13 @@
 #    - 그래프 연결 성분 찾기
 
 
-def dfs(graph, start_node):
-    visited = [False] * (len(graph) + 1)
+def dfs(graph, start_node,visited):
     visited[start_node] = True
     print(start_node)
     
     for adj_node in graph[start_node]: # 인접한 노드 방문
        if not visited[adj_node]:
-          dfs(graph,adj_node)
+          dfs(graph,adj_node,visited)
               
 
 # 그래프를 인접 리스트로 표현
@@ -40,5 +39,6 @@ graph = {
     5: []
 }
 
+visited = [False] * (len(graph) + 1)
 # DFS 알고리즘 실행
-dfs(graph, 1) # 1 2 4 5 3
+dfs(graph, 1,visited) # 1 2 4 5 3
