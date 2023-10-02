a23e93a5f41382526b23b520ebcbbea5d4a3d180
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-10-02 22:00:02 +0900

    Update dfs_stack.py

--- Algorithm_with_DataStructure/dfs_stack.py
+++ Algorithm_with_DataStructure/dfs_stack.py
@@ -21,7 +21,6 @@
 #    - 그래프 연결 성분 찾기
 
 
-
 def dfs(graph, start_node):
     visited = [False] * (len(graph) + 1)
     stack = [start_node]
@@ -33,8 +32,22 @@ def dfs(graph, start_node):
             visited[node] = True;
             print(node)
             for adj_node in graph[node]: # 인접한 노드 방문
-                    if not visited[adj_node[:
+                    if not visited[adj_node]:
                         stack.append(adj_node)
+              
+
+# 그래프를 인접 리스트로 표현
+graph = {
+    1: [2, 3],
+    2: [4, 5],
+    3: [],
+    4: [],
+    5: []
+}
+
+# DFS 알고리즘 실행
+dfs(graph, 1) # 1 3 2 5 4
+
               
 
 # 그래프를 인접 리스트로 표현
