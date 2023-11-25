f6056b3ffd23ec1ac64c28be153224cf3924a431
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-11-25 21:36:23 +0900

    Update 01.py

--- solution/01.py
+++ solution/01.py
@@ -1,3 +1,9 @@
 def solution(arr):
-  arr.reverse( )
+  arr.sort()
   return arr
+  
+
+# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
+# print(solution([1,-5,2,4,3])) # [-5, 1, 2, 3, 4]
+# print(solution([2,1,1,3,2,5,4])) # [1, 1, 2, 2, 3, 4, 5]
+# print(solution([1,6,7])) # [1, 6, 7]
