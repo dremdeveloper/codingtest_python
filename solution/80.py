cf0adfc6a776dbb289fbc79eac73301abd5f3cd9
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-11-25 22:54:39 +0900

    Update 80.py

--- solution/80.py
+++ solution/80.py
@@ -10,3 +10,8 @@ def solution(amount):
       amount -= coin  # ❺ 정산이 완료된 거스름돈 뺌
   # ❻ 거스름돈 리스트 반환
   return change
+
+
+# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
+# print(solution(123)) # 반환값 : [100, 10, 10, 1, 1, 1]
+# print(solution(350)) # 반환값 : [100, 100, 100, 50]
