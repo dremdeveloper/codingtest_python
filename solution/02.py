91811c5c3d2c0ab413dbc55edec6cabe9a683d8d
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-11-25 21:38:22 +0900

    Update 02.py

--- solution/02.py
+++ solution/02.py
@@ -2,3 +2,7 @@ def solution(lst):
   unique_lst = list(set(lst)) # ➊ 중복값 제거
   unique_lst.sort(reverse=True) # ➋ 내림차순 정렬 
   return unique_lst
+  
+# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
+# print(solution([4, 2, 2, 1, 3, 4])) # [4, 3, 2, 1]
+# print(solution([2, 1, 1, 3, 2, 5, 4])) # [5, 4, 3, 2, 1]
