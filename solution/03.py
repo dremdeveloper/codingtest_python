43f6abad7f6dc85ac1a4f6ca2b8d86980f48eae4
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-11-25 21:45:19 +0900

    Update 03.py

--- solution/03.py
+++ solution/03.py
@@ -10,5 +10,5 @@ def solution(numbers):
   return ret  # ➎ 최종 결과 반환
 
 #TEST 코드입니다. 주석을 풀어서 확인해보세요
-# print(solution([2, 1, 3, 4, 1])) #[2, 3, 4, 5, 6, 7]
-# print(solution([5, 0, 2, 7])) #[2, 5, 7, 9, 12]
+# print(solution([2, 1, 3, 4, 1])) # 반환값 : [2, 3, 4, 5, 6, 7]
+# print(solution([5, 0, 2, 7])) # 반환값 : [2, 5, 7, 9, 12]
