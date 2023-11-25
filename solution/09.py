d04d86aa14c3cbf84f6103cf3347000c58b7522d
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-11-25 21:44:45 +0900

    Update 09.py

--- solution/09.py
+++ solution/09.py
@@ -6,3 +6,9 @@ def solution(decimal):
         decimal //= 2
     stack.reverse()
     return ''.join(stack)
+    
+    
+# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
+# print(solution(10)) #반환값 :  1010
+# print(solution(27)) #반환값 :  11011
+# print(solution(12345)) #반환값 : 11000000111001
