8907090a035dee844de133a26f7984fd71c779c3
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-11-25 21:45:32 +0900

    Update 02.py

--- solution/02.py
+++ solution/02.py
@@ -4,5 +4,5 @@ def solution(lst):
   return unique_lst
   
 # TEST 코드 입니다. 주석을 풀고 실행시켜보세요
-# print(solution([4, 2, 2, 1, 3, 4])) # [4, 3, 2, 1]
-# print(solution([2, 1, 1, 3, 2, 5, 4])) # [5, 4, 3, 2, 1]
+# print(solution([4, 2, 2, 1, 3, 4])) # 반환값 : [4, 3, 2, 1]
+# print(solution([2, 1, 1, 3, 2, 5, 4])) # 반환값 : [5, 4, 3, 2, 1]
