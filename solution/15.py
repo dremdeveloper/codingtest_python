08278d786f8b1f60635235f028067e0545740528
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2023-12-06 21:20:53 +0900

    Update 15.py

--- solution/15.py
+++ solution/15.py
@@ -7,8 +7,8 @@ def solution(N, K):
     while len(queue) > 1: #❷ deque에 하나의 요소가 남을 때까지
         for _ in range(K-1):
             queue.append(queue.popleft()) #❸ K번째 요소를 찾기 위해 앞에서부터 제거하고 뒤에 추가
-            queue.popleft() #❹ K번째 요소 제거
+        queue.popleft() #❹ K번째 요소 제거
     return queue[0] #❺ 마지막으로 남은 요소 반환
-
-
+    
+    
 print(solution(5,2))
